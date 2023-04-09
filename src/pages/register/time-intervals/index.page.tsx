f64a414b10f0e387570b0cdcda9f86063675c4cc
gabriel bauer <gabriel.bauer9@hotmail.com>
2023-04-09 10:24:12 -0300

    feat: disponibility page

---
+++ src/pages/register/time-intervals/index.page.tsx
@@ -0,0 +1,65 @@
+import {
+  Heading,
+  Text,
+  MultiStep,
+  Checkbox,
+  TextInput,
+  Button,
+} from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+import {
+  IntervalBox,
+  IntervalDay,
+  IntervalInputs,
+  IntervalItem,
+  IntervalsContainer,
+} from './styles'
+import { ArrowRight } from 'phosphor-react'
+
+export default function TimeIntervals() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Quase lá</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+
+        <MultiStep size={4} currentStep={3} />
+      </Header>
+
+      <IntervalBox as="form">
+        <IntervalsContainer>
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Segunda-feira</Text>
+            </IntervalDay>
+
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Terça-feira</Text>
+            </IntervalDay>
+
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+        </IntervalsContainer>
+
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </IntervalBox>
+    </Container>
+  )
+}
